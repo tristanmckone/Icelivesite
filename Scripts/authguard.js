ae5ec1d13ad11f3e535e2c6d39c00814d862c223
Tristan Mckone <tristanmckone@gmail.com>
2022-03-11 15:14:37 -0500

    site

--- Scripts/authguard.js
+++ Scripts/authguard.js
@@ -1,19 +1,12 @@
 "use strict";
-(function()
-{
- // if user does not exist in the session storage
- 
-     let protected_routes = ["contact-list"
+(function () {
+    let protected_routes = [
+        "contact-list"
     ];
-
-    if(protected_routes.indexOf(router.ActiveLink) > -1)
-    {
-        if(!sessionStorage.getItem("user"))
-        {
-             // redirect to the login page
+    if (protected_routes.indexOf(router.ActiveLink) > -1) {
+        if (!sessionStorage.getItem("user")) {
             location.href = "/login";
         }
     }
-    
- 
 })();
+//# sourceMappingURL=authguard.js.map
--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,12 +1,33 @@
+"use strict";
+var core;
 (function (core) {
     class Contact {
-        // constructor
-        constructor(fullName = "", contactNumber = "", emailAddress = "") {
-            this.FullName = fullName;
-            this.ContactNumber = contactNumber;
-            this.EmailAddress = emailAddress;
+        m_fullName;
+        m_contactNumber;
+        m_emailAddress;
+        get FullName() {
+            return this.m_fullName;
         }
-        // public methods
+        set FullName(full_name) {
+            this.m_fullName = full_name;
+        }
+        get ContactNumber() {
+            return this.m_contactNumber;
+        }
+        set ContactNumber(contact_number) {
+            this.m_contactNumber = contact_number;
+        }
+        get EmailAddress() {
+            return this.m_emailAddress;
+        }
+        set EmailAddress(email_address) {
+            this.m_emailAddress = email_address;
+        }
+        constructor(fullName = "", contactNumber = "", emailAddress = "") {
+            this.m_fullName = fullName;
+            this.m_contactNumber = contactNumber;
+            this.m_emailAddress = emailAddress;
+        }
         serialize() {
             if (this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "") {
                 return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
@@ -20,10 +41,10 @@
             this.ContactNumber = propertyArray[1];
             this.EmailAddress = propertyArray[2];
         }
-        // overridden methods
         toString() {
             return `Full Name     : ${this.FullName}\nContact Number: ${this.ContactNumber}\nEmail Address : ${this.EmailAddress}`;
         }
     }
     core.Contact = Contact;
 })(core || (core = {}));
+//# sourceMappingURL=contact.js.map
